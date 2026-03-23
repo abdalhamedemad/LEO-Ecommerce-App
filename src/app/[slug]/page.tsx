dd03ffcf2c20c293983d167e1b2ed0740d34ac33
abdalhamedemad <76442606+abdalhamedemad@users.noreply.github.com>
2026-03-24 01:16:31 +0200

    feat: add product info page

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -3,60 +3,67 @@ import Container from "@/components/BasicComponents/Container";
 import React from "react";
 import CustomizeProducts from "@/components/SinglePage/CustomizeProducts";
 import Add from "@/components/SinglePage/Add";
+import { WixClientServer } from "@/lib/wixClientServer";
+import { notFound } from "next/navigation";
 
-const SinglePage = () => {
+const SinglePage = async ({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) => {
+  const { slug } = await params;
+  console.log("slug", slug);
+  const wixClient = await WixClientServer();
+  const products = await wixClient.products
+    .queryProducts()
+    .eq("slug", slug)
+    .find();
+  console.log("product", products);
+  if (!products?.items[0]) {
+    return notFound();
+  }
+  const product = products?.items[0];
   return (
     <Container>
       <div className="relative flex flex-col lg:flex-row gap-16">
         {/* IMG */}
         <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
-          <ProductImages />
+          <ProductImages items={product?.media?.items} />
         </div>
         {/* Text */}
         <div className="w-full lg:w-1/2 flex flex-col gap-6">
-          <h1 className="text-4xl font-medium">Product Name</h1>
-          <p className="text-gray-500">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
-            veritatis illo obcaecati numquam deleniti laudantium quaerat hic
-            cumque odit, nesciunt assumenda, repellat dicta esse. Facere a
-            dignissimos illum delectus illo!
-          </p>
+          <h1 className="text-4xl font-medium">{product.name}</h1>
+          <p
+            className="text-gray-500"
+            dangerouslySetInnerHTML={{ __html: product.description }}
+          ></p>
           <div className="h-[2px] bg-gray-100" />
           <div className="flex items-center gap-4">
-            <h3 className="text-xl text-gray-500 line-through">$59</h3>
-            <h2 className="text-2xl font-medium">$49</h2>
+            {product.price?.price === product.price?.discountedPrice ? (
+              <h3 className="text-2xl font-medium">${product.price?.price}</h3>
+            ) : (
+              <>
+                <h3 className="text-xl text-gray-500 line-through">
+                  ${product.price?.price}
+                </h3>
+                <h2 className="text-2xl font-medium">
+                  ${product.price?.discountedPrice}
+                </h2>
+              </>
+            )}
           </div>
           <div className="h-[2px] bg-gray-100" />
           <CustomizeProducts />
           <Add />
           <div className="h-[2px] bg-gray-100" />
-          <div className="text-sm">
-            <h4 className="font-medium mb-4">PRODUCT INFO</h4>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
-              suscipit quaerat vel mollitia molestiae voluptatum eos minus
-              blanditiis. Itaque, nemo! In beatae at voluptates fuga cupiditate
-              ex, consectetur sit eveniet.
-            </p>
-          </div>
-          <div className="text-sm">
-            <h4 className="font-medium mb-4">RETURN & FUND POLICY</h4>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
-              suscipit quaerat vel mollitia molestiae voluptatum eos minus
-              blanditiis. Itaque, nemo! In beatae at voluptates fuga cupiditate
-              ex, consectetur sit eveniet.
-            </p>
-          </div>
-          <div className="text-sm">
-            <h4 className="font-medium mb-4">SHIPPING INFO</h4>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
-              suscipit quaerat vel mollitia molestiae voluptatum eos minus
-              blanditiis. Itaque, nemo! In beatae at voluptates fuga cupiditate
-              ex, consectetur sit eveniet.
-            </p>
-          </div>
+          {product.additionalInfoSections?.map((section: any) => (
+            <div className="text-sm" key={section.title}>
+              <h4 className="font-lg mb-4 uppercase font-bold">
+                {section.title}
+              </h4>
+              <p dangerouslySetInnerHTML={{ __html: section.description }}></p>
+            </div>
+          ))}
         </div>
       </div>
     </Container>
--- src/components/SinglePage/ProductImages.tsx
+++ src/components/SinglePage/ProductImages.tsx
@@ -19,14 +19,14 @@ const images = [
     url: "https://images.pexels.com/photos/34794612/pexels-photo-34794612.jpeg",
   },
 ];
-const ProductImages = () => {
+const ProductImages = ({ items }: { items: any }) => {
   const [mainImageIndex, setMainImageIndex] = useState(0);
   return (
     <div>
       <div className="h-[500px] relative">
         {/* size 50vw means the image will take up 50% of the viewport width i.e size is the max width*/}
         <Image
-          src={images[mainImageIndex].url}
+          src={items[mainImageIndex].image?.url}
           alt="Product Image"
           fill
           sizes="50vw"
@@ -35,10 +35,10 @@ const ProductImages = () => {
         />
       </div>
       <div className="flex justify-between gap-4 mt-8">
-        {images.map((image, index) => (
-          <div key={image.id} className="w-1/4 h-32 relative cursor-pointer ">
+        {items.map((item: any, index: number) => (
+          <div key={item._id} className="w-1/4 h-32 relative cursor-pointer ">
             <Image
-              src={image.url}
+              src={item?.image?.url}
               alt={`Product Image ${index + 1}`}
               fill
               sizes="30vw"
